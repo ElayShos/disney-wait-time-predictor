4ed46ec5c236683dafa7a6540a8d375b0b3128a8
ElayShos <elayshostak@gmail.com>
2026-03-13 20:00:43 +0200

    fix

--- collect_data.py
+++ collect_data.py
@@ -1,69 +1,119 @@
 import requests
 import pandas as pd
 from datetime import datetime
-from zoneinfo import ZoneInfo
 import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
-# ensure data folder exists
-os.makedirs("data", exist_ok=True)
-
-# park ids
 mk = "75ea578a-adc8-4116-a54d-dccb60765ef9"
 ep = "47f90d2c-e191-4239-a466-5892ef59a88b"
 hs = "288747d1-8b4f-4a64-867e-ea7c9b27bad8"
 ak = "1c84a229-8862-4648-9c71-378ddd2c7693"
 
+mkurl = "https://api.themeparks.wiki/v1/entity/" + mk + "/live"
+epurl = "https://api.themeparks.wiki/v1/entity/" + ep + "/live"
+hsurl = "https://api.themeparks.wiki/v1/entity/" + hs + "/live"
+akurl = "https://api.themeparks.wiki/v1/entity/" + ak + "/live"
+
 parks = {
-    "mk": mk,
-    "ep": ep,
-    "hs": hs,
-    "ak": ak
+    mkurl: "mk",
+    epurl: "ep",
+    hsurl: "hs",
+    akurl: "ak"
 }
 
-base_url = "https://api.themeparks.wiki/v1/entity/{}/live"
+rides = {
+    "mk": {
+        "TRON Lightcycle / Run"
+        "Seven Dwarfs Mine Train"
+        "Jungle Cruise",
+        "Tiana's Bayou Adventure",
+        "Peter Pan's Flight",
+        "Big Thunder Mountain Railroad",
+        "Space Mountain",
+        "The Barnstormer",
+        "Dumbo the Flying Elephant",
+        "Haunted Mansion",
+        "it's a small world",
+        "The Magic Carpets of Aladdin",
+        "Mad Tea Party",
+        "Mickey's PhilharMagic",
+        "The Many Adventures of Winnie the Pooh",
+        "Pirates of the Caribbean",
+        "Tomorrowland Speedway",
+        "Under the Sea - Journey of The Little Mermaid",
+        "Monsters Inc. Laugh Floor",
+        "Buzz Lightyear's Space Ranger Spin"
+    },
 
-# timestamp in Orlando time
-timestamp = datetime.now(ZoneInfo("America/New_York")).replace(second=0, microsecond=0)
+    "ep": {
+        "Test Track",
+        "Guardians of the Galaxy: Cosmic Rewind",
+        "Remy's Ratatouille Adventure",
+        "Soarin'",
+        "Living with the Land",
+        "Spaceship Earth",
+        "Mission: SPACE",
+        "Frozen Ever After",
+        "Gran Fiesta Tour Starring The Three Caballeros",
+        "The Seas with Nemo & Friends"
+    },
 
-rows = []
+    "hs": {
+        "Star Wars: Rise of the Resistance",
+        "Rock 'n' Roller Coaster Starring Aerosmith",
+        "The Twilight Zone Tower of Terror",
+        "Millennium Falcon: Smugglers Run",
+        "Slinky Dog Dash",
+        "Mickey & Minnie's Runaway Railway",
+        "Toy Story Mania!",
+        "Star Tours – The Adventures Continue"
+    },
 
-for park_name, park_id in parks.items():
+    "ak": {
+        "Expedition Everest - Legend of the Forbidden Mountain",
+        "Avatar Flight of Passage",
+        "Na'vi River Journey",
+        "Kilimanjaro Safaris",
+        "Kali River Rapids"
+    }
+}
 
-    url = base_url.format(park_id)
-    data = requests.get(url).json()
+wdw = [mkurl, epurl, hsurl, akurl]
+
+timestamp = datetime.now(ZoneInfo("America/New_York"))
+
+for park in wdw:
+
+    row = {}
+
+    data = requests.get(park).json()
 
     for ride in data["liveData"]:
 
         if ride.get("entityType") == "ATTRACTION":
 
-            queue = ride.get("queue", {})
-            standby = queue.get("STANDBY", {})
+            ride_name = ride["name"]
 
-            wait_time = standby.get("waitTime")
-
-            if wait_time is None:
+            if ride_name not in rides[park_name]:
                 continue
 
-            rows.append({
-                "timestamp": timestamp,
-                "park": park_name.upper(),
-                "ride": ride["name"],
-                "wait_time": wait_time,
-                "hour": timestamp.hour,
-                "day_of_week": timestamp.weekday()
-            })
+            if "queue" in ride and "STANDBY" in ride["queue"]:
 
-print("Collected rides:", len(rows))
+                wait_time = ride["queue"]["STANDBY"]["waitTime"]
 
-df = pd.DataFrame(rows)
+                row[ride_name] = wait_time if wait_time else None
 
-file = "data/wait_times.csv"
+    df = pd.DataFrame([row], [timestamp])
 
-df.to_csv(
-    file,
-    mode="a",
-    header=not os.path.exists(file),
-    index=False
-)
+    park_name = parks[park]
 
-print("Saved data at:", timestamp)
+    file = "data/wait"+ park_name.upper() +".csv"
+
+    if os.path.exists(file):
+        df.to_csv(file, mode="a", header=False)
+    else:
+        df.to_csv(file)
+    print("Data collected:", len(row))
+
+print("Collected Data for all parks at", timestamp)
