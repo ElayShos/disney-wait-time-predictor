67dc90ccb90f1550647b50d2c825adb462da69ea
ElayShos <elayshostak@gmail.com>
2026-03-17 22:22:21 +0200

    update

--- predict.py
+++ predict.py
@@ -1,5 +1,9 @@
 import numpy as np
+import pandas as pd
 import train_model
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
 
 def main():
     res = train_model.train_model()
@@ -7,7 +11,7 @@ def main():
     mapping = res['mapping']
     m = res['metrics']
     
-    print(f"MAE: {m['mae']:.2f} min | R2: {m['r2']:.2%}")
+    print(f"MAE (Margin of Error): {m['mae']:.2f} min | R2 (Accuracy): {m['r2']:.2%}")
 
     names = list(mapping.keys())
 
@@ -38,7 +42,10 @@ def main():
         s = np.sin(2 * np.pi * h / 24.0)
         c = np.cos(2 * np.pi * h / 24.0)
         
-        pred = model.predict([[d, s, c, r_id]])[0]
+        cols = ['day_of_week', 'hour_sin', 'hour_cos', 'ride_id']
+        sample = pd.DataFrame([[d, s, c, r_id]], columns=cols)
+        
+        pred = int(round(model.predict(sample)[0]))
         
         print(f"Result for {target.upper()}: {pred:.1f}m (+/- {m['mae']:.1f})")
 
